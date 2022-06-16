d946401d3d02c30a4a17ba8bf8ad422127c01594
Quastrado <Ataxyswriter@yandex.ru>
2022-06-16 22:42:25 +0300

    token hiding

--- firefoxdriver/main.py
+++ firefoxdriver/main.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from fake_useragent import UserAgent
+from secret.secret import gh_token
 import os
 import time
 
-os.environ['GH_TOKEN'] = "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"
+os.environ['GH_TOKEN'] = gh_token
 url = 'https://www.vk.com'
 user_agent_url = 'http://n5m.ru/usagent.html'
 options = webdriver.FirefoxOptions()
