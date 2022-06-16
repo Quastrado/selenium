de76571db78b91b0897d72b07efd10d97f10a5c3
Quastrado <Ataxyswriter@yandex.ru>
2022-06-16 20:20:57 +0300

    useragent change

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from fake_useragent import UserAgent
 import time
 
 url = 'https://www.google.com'
 user_agent_url = 'http://n5m.ru/usagent.html'
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = webdriver.ChromeOptions()
+user_agent = UserAgent()
+options.add_argument(f'user-agent={user_agent.random}')
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 
 try:
     driver.get(url=user_agent_url)
--- firefoxdriver/main.py
+++ firefoxdriver/main.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from fake_useragent import UserAgent
 import os
 import time
 
 os.environ['GH_TOKEN'] = "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"
 url = 'https://www.vk.com'
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+user_agent_url = 'http://n5m.ru/usagent.html'
+options = webdriver.FirefoxOptions()
+user_agent = UserAgent()
+options.set_preference('general.useragent.override', user_agent.random)
+driver = webdriver.Firefox(
+    service=Service(GeckoDriverManager().install()),
+    options=options
+)
 
 try:
-    driver.get(url=url)
+    driver.get(url=user_agent_url)
     time.sleep(3)
 except Exception as ex:
     print(ex)
